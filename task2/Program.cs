47590cd26df43953177243745a9d7e87736dcf3e
Dmytro Kostiuchenko <dmyk@nuuday.dk>
2021-06-23 17:38:28 +0300

    Task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -6,7 +6,23 @@ namespace task2
     {
         private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            MyClass.Method1();
+            //MyClass.Method2();
+        }
+    }
+
+    internal class MyClass
+    {
+        [Obsolete("", error:false)]
+        public static void Method1()
+        {
+            Console.WriteLine("Method1");
+        }
+
+        [Obsolete("", error: true)]
+        public static void Method2()
+        {
+            Console.WriteLine("Method1");
         }
     }
 }
